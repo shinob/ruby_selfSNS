1efba7cad47e83836c828ebd28b1f22a21c9b347
shinob <shinob@mbb.nifty.ne.jp>
2015-09-23 15:01:03 +0900

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ruby_selfSNS
-The social network system made by ruby
+
+The social network system made by ruby.
+
+## How to setup
+
+git clone https://github.com/shinob/ruby_selfSNS.git [your directory name]
+cd [your directory name]
+sh setup.sh
+
+## How to startup
+
+cd [your directory name]
+ruby server.rb
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+libdir="selfSNS/lib"
+appname="selfSNS"
+wwwuser="apache"
+
+## データベースファルの作成
+
+sqlite3 ${libdir}/databases/${appname}.db < ${libdir}/databases/${appname}.txt
+
+## ファイル保存フォルダの作成
+
+mkdir ${appname}/files
+chown apache ${appname}/files
+
+# Root権限がない場合
+# chmod 777 ${appname}/files
+
+## ファイル属性の変更
+
+chmod 755 ${appname}/*.rb
+chown ${wwwuser} ${libdir}/databases
+chown ${wwwuser} ${libdir}/databases/${appname}.db
+
+# Root権限がない場合
+# chmod 777 ${libdir}/databases
+# chmod 777 ${libdir}/databases/${appname}.db
+
+## ログファイルの作成
+
+touch ${libdir}/login.log
+chown apache ${libdir}/login.log
+
+# Root権限がない場合
+# chmod 666 ${libdir}/login.log
